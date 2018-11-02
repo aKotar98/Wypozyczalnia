93cde8999e09d1b002fc95b69c5c9cf50fff8642
Adam Kotarski <kotarskiadam65@gmail.com>
2018-11-02 12:27:10 +0100

    Next commit

---
+++ Wypozyczalnia/Menu.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include "Menu.h"
+#include <iostream>
+#include <Windows.h>
+
+
+Menu::Menu()
+{
+}
+
+
+Menu::~Menu()
+{
+}
+
+void Menu::ShowMenu() {
+	char z;
+
+	FILE *file;
+	file = fopen("Menu.txt", "r");
+	if (file == NULL) printf("Blad otwarcia pliku\n");
+	else
+	{
+		while (true)
+		{
+			if (feof(file)) break; /*jeœli koniec pliku koniec dzia³ania*/
+			fscanf(file, "%c", &z);
+			printf(" %c", z);
+			Sleep(5);
+		}
+		std::cout << "\n";
+	}
+}
